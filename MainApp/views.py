76786ab45d88f91f94ca5cf8e83313d859ccdecf
Alexey <geniy73@gmail.com>
2024-07-22 16:23:02 +0300

    /about problem

--- MainApp/views.py
+++ MainApp/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
+
 def home(request):
     text="""
     <h1>"Изучаем django"</h1>
@@ -12,14 +14,15 @@ def home(request):
 
 def about(request):
     firstname = "Иван"
-    surname="Петрович"
-    lastname="Иванов"
-    phonenumber="8-923-600-01-02"
-    email="ivan@mail.ru"
-    text="""f'<p1>Имя:<strong>{firstname}</strong></p>/
-                <p>Отчество:<strong>{surname}</strong></p>/
-                <p>Фамилия:<strong>{lastname}</strong></p>/
-                <p>Телефон:<strong>{phonenumber}</strong></p>/
-                <p>Эл.почта:<strong>{email}</strong></p>'
+    surname = "Петрович"
+    lastname = "Иванов"
+    phonenumber = "8-923-600-01-02"
+    email = "ivan@mail.ru"
+    
+    text="""f"<p>Имя:<strong>{firstname}</strong></p>
+                <p>Отчество:<strong>{surname}</strong></p>
+                <p>Фамилия:<strong>{lastname}</strong></p>
+                <p>Телефон:<strong>{phonenumber}</strong></p>
+                <p>Эл.почта:<strong>{email}</strong></p>"
         """
     return HttpResponse(text)
