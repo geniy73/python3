274aa61c41f69c27b91899ac1c486a134a28fea5
Alexey <geniy73@gmail.com>
2024-07-22 16:56:05 +0300

    /about OK

--- MainApp/views.py
+++ MainApp/views.py
@@ -2,6 +2,13 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+author = {
+    "Имя" : "Иван",
+    "Отчество" : "Петрович",
+    "Фамилия" : "Иванов",
+    "Телефон" : "8-923-600-01-02",
+    "email" : "ivan@mail.ru",
+}
 
 
 
@@ -13,15 +20,18 @@ def home(request):
     return HttpResponse(text)
 
 def about(request):
-    firstname = "Иван"
-    surname = "Петрович"
-    lastname = "Иванов"
-    phonenumber = "8-923-600-01-02"
-    email = "ivan@mail.ru"
     
-    text=f"""<p>Имя:<strong>{firstname}</strong></p>
-                <p>Отчество:<strong>{surname}</strong></p>
-                <p>Фамилия:<strong>{lastname}</strong></p>
-                <p>Телефон:<strong>{phonenumber}</strong></p>
-                <p>Эл.почта:<strong>{email}</strong></p>"""
+    text=f"""
+    <p>Имя: <strong>{author["Имя"]}</strong></p>
+    <p>Отчество: <strong>{author["Отчество"]}</strong></p>
+    <p>Фамилия: <strong>{author["Фамилия"]}</strong></p>
+    <p>Телефон: <strong>{author["Телефон"]}</strong></p>
+    <p>Эл.почта: <strong>{author["email"]}</strong></p>
+    """
+    return HttpResponse(text)
+
+def item(request):
+    
+    text = "test"
+
     return HttpResponse(text)
