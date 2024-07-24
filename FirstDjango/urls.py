19e1674a68cd82d6c80d7599dea87cbb2cf75197
Alexey <geniy73@gmail.com>
2024-07-24 11:14:37 +0300

    Mod 4 task 3

--- FirstDjango/urls.py
+++ FirstDjango/urls.py
@@ -5,7 +5,7 @@ from django.urls import path
 urlpatterns = [
     path('', views.home),
     path('about', views.about),
-    path('item/<int:item_id>', views.get_item),
-    path('items', views.get_items),
+    path('item/<int:item_id>', views.get_item, name="item-detail"),
+    path('items', views.items_list, name="items-list"),
 
 ]
--- MainApp/views.py
+++ MainApp/views.py
@@ -5,21 +5,15 @@ from .models import Item
 from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
-author = {
-    "Имя" : "Иван",
-    "Отчество" : "Петрович",
-    "Фамилия" : "Иванов",
-    "Телефон" : "8-923-600-01-02",
-    "email" : "ivan@mail.ru",
-}
+# author = {
+#     "Имя" : "Иван",
+#     "Отчество" : "Петрович",
+#     "Фамилия" : "Иванов",
+#     "Телефон" : "8-923-600-01-02",
+#     "email" : "ivan@mail.ru",
+# }
 
-# items = [
-#    {"id": 1, "name": "Кроссовки abibas" ,"quantity":5},
-#    {"id": 2, "name": "Куртка кожаная" ,"quantity":2},
-#    {"id": 5, "name": "Coca-cola 1 литр" ,"quantity":12},
-#    {"id": 7, "name": "Картофель фри" ,"quantity":0},
-#    {"id": 8, "name": "Кепка" ,"quantity":124},
-# ]
+
 
 def home(request):
     context = {
@@ -40,13 +34,15 @@ def about(request):
 
 def get_item(request, item_id):
     """ По указанному id возвращаем имя и кол-во элемента """
-    if Item.objects.get(id=item_id).id == item_id:
-        context = {
-                "item": Item.objects.get(id=item_id)
-                    }  
-        return render(request, 'item_page.html', context)
+    try:
+        item = Item.objects.get(id=item_id)
+    except ObjectDoesNotExist:
+        return render(request, "errors.html", {"error": f"Item with id={item_id} not found."})
+    else:
+        context= {"item": item}
+        return render(request, "item_page.html", context)
 
-def get_items(request):
+def items_list(request):
     context = {
         "items": Item.objects.all()
     }
