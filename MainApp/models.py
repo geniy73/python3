753abc4fb81aa0507ed6ecb00d76263660accfb3
Alexey <geniy73@gmail.com>
2024-07-24 09:59:12 +0300

    db p.2

--- MainApp/models.py
+++ MainApp/models.py
@@ -4,4 +4,5 @@ from django.db import models
 class Item(models.Model):
     name  = models.CharField(max_length=100)
     brand = models.CharField(max_length=100)
-    count = models.PositiveIntegerField() 
+    count = models.PositiveIntegerField()
+    description = models.CharField(max_length=300,default='') 
